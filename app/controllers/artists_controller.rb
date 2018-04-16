628e05206e133592d1548a1cdcdad61289abf33b
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-16 05:39:58 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,9 +9,13 @@ class ArtistsController < ApplicationController
   end
 
   def create
+<<<<<<< HEAD
     @artist = Artist.new(artist_params)
     @artist.save
     redirect_to artist_path(@artist)
+=======
+    @artist = Artist.new
+>>>>>>> 0191dc00b5737d48d15c839828b961477530c19f
   end
 
   def edit
@@ -20,13 +24,19 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
+<<<<<<< HEAD
     @artist.update(artist_params)
     redirect_to artist_path(@artist)
+=======
+>>>>>>> 0191dc00b5737d48d15c839828b961477530c19f
   end
 
   private
 
   def artist_params
     params.require(:artist).permit(:name, :bio)
+<<<<<<< HEAD
   end
+=======
+>>>>>>> 0191dc00b5737d48d15c839828b961477530c19f
 end
